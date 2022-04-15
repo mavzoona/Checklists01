18087b0a2dbdf1ceb99993c2255bf43794aaaed4
Mavzuna Khairullo <mavzuna_24@mail.ru>
2022-04-15 17:54:03 +0500

    just creat
    just creat

---
+++ Checklists01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists01
+//
+//  Created by ASDiMac1 on 15/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var greeting: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
