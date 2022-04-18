8cea2cb1258d20d10caec4bf5cb54b5f6016c166
Mavzuna Khairullo <mavzuna_24@mail.ru>
2022-04-18 18:14:24 +0500

    creat checklists

--- Checklists01/MainViewContriller.swift
+++ Checklists01/MainViewContriller.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class MainViewContriller: UITableViewController {
     
+    let groups: [CheckListGroup] = [
+        CheckListGroup (title: "Birthdays", imageName: "Birthdaypic"),
+        CheckListGroup(title: "Groceries", imageName: "groceriespic"),
+        CheckListGroup(title: "To do", imageName: "todotopic"),
+        CheckListGroup(title: "Business Stuff", imageName: "pic")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let group: CheckListGroup = groups[indexPath.row]
+        let cell = UITableViewCell()
+        cell.textLabel?.text = group.title
+        return cell
+    }
 }
 
---
+++ Checklists01/Models/CheckListGroup.swift
@@ -0,0 +1,13 @@
+//
+//  CheckListGroup.swift
+//  Checklists01
+//
+//  Created by ASDiMac1 on 18/04/22.
+//
+
+import Foundation
+
+struct CheckListGroup {
+    let  title: String
+    let imageName: String
+}
